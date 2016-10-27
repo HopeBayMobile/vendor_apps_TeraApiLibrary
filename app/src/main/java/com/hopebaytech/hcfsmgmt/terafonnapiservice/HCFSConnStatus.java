635c8ceaf680e79d102c0b6ae22e6a094a1284ff
daniel_wei <daniel.wei@hopebaytech.com>
2016-10-27 11:53:27 +0800

    Add HCFSConnStatus class

---
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/terafonnapiservice/HCFSConnStatus.java
@@ -0,0 +1,13 @@
+package com.hopebaytech.hcfsmgmt.terafonnapiservice;
+
+/**
+ * Created by daniel on 2016/10/21.
+ */
+
+public class HCFSConnStatus {
+    public static final int TRANS_FAILED = -1;
+    public static final int TRANS_NOT_ALLOWED = 0;
+    public static final int TRANS_NORMAL = 1;
+    public static final int TRANS_IN_PROGRESS = 2;
+    public static final int TRANS_SLOW = 3;
+}
