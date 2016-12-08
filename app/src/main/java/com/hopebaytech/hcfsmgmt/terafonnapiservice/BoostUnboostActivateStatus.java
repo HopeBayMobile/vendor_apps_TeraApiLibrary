d7955c57b2ed9576feda6187fc33c21b6aebd461
Aaron <aaron.chu@happygorgi.com>
2016-12-08 16:56:18 +0800

    Add BoostUnboostActivateStatus.aidl

---
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/terafonnapiservice/BoostUnboostActivateStatus.java
@@ -0,0 +1,24 @@
+package com.hopebaytech.hcfsmgmt.terafonnapiservice;
+
+/**
+ * @author Aaron
+ *         Created by Aaron on 2016/12/8.
+ */
+public class BoostUnboostActivateStatus {
+
+    /**
+     * Not boost or unboost is activated.
+     */
+    public static final int NOT_BOOST_UNBOOST = 0;
+
+    /**
+     * Unboost is activated.
+     */
+    public static final int UNBOOST_ACTIVATED = 1;
+
+    /**
+     * Boost is activated.
+     */
+    public static final int BOOST_ACTIVATED = 2;
+
+}
