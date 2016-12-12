42f00d345cbf6aa547327e5cd2857a5a7f930f50
Aaron <aaron.chu@happygorgi.com>
2016-12-12 11:32:24 +0800

    Add ViewPage.java for Launcher or Ssystem Settings to specify which page to move when open Tera app

---
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/terafonnapiservice/ViewPage.java
@@ -0,0 +1,18 @@
+package com.hopebaytech.hcfsmgmt.terafonnapiservice;
+
+/**
+ * @author Aaron
+ *         Created by Aaron on 2016/12/12.
+ */
+public class ViewPage {
+
+    public static final String KEY = "view_page";
+
+    public static final int NONE = -1;
+    public static final int OVERVIEW = 0;
+    public static final int APP = 1;
+    public static final int FILE = 2;
+    public static final int SETTINGS = 3;
+    public static final int BOOSTER = 4;
+    public static final int HELP = 5;
+}
